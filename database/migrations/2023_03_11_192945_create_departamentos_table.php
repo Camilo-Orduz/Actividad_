5d25d9f7f12c304dbb79b9b4de158ab369b8553d
Camilo-Orduz <91356275+Camilo-Orduz@users.noreply.github.com>
2023-03-12 08:28:33 -0500

    Initial commit

---
+++ database/migrations/2023_03_11_192945_create_departamentos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->increments('id_departamentos');
+            $table->string('nombre_departamento', 125);
+            $table->unsignedInteger('pais_id');
+            $table->foreign('pais_id')->references('id_paises')->on('paises')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departamentos');
+    }
+};
---
+++ database/migrations/2023_03_11_193526_create_municipios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->increments('id_municipios');
+            $table->string('nombre_municipio');
+            $table->unsignedInteger('departamento_id');
+            $table->foreign('departamento_id')->references('id_departamentos')->on('departamentos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('municipios');
+    }
+};
